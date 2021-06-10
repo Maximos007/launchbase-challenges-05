f018c6ec74a5f65417c4b0f5596dfdc21cc9164e
Maximos007 <waslleymax@gmail.com>
2021-06-10 00:57:16 -0300

    Update utils.js
    bug fixed

--- 5-2-Interagindo-com-o-BD/src/lib/utils.js
+++ 5-2-Interagindo-com-o-BD/src/lib/utils.js
@@ -17,7 +17,7 @@ module.exports = {
 		const date = new Date(timestamp);
 		const year = date.getUTCFullYear();
 		const month = `0${date.getUTCMonth() + 1}`.slice(-2);
-		const day = `0${date.getUTCDay()}`.slice(-2);
+		const day = `0${date.getUTCDate()}`.slice(-2);
 
 		return {
 			day,
